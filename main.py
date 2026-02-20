1a9fc9c304417bc370ef869d9200d8183babbd93
KowDaw <kov.dav.23@gmail.com>
2026-02-20 15:43:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,82 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import smtplib
+import pandas
+import datetime as dt
+from random import randint
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+def get_today_month_and_today_day():
+    current_date = dt.datetime.now()
+    return (current_date.month, current_date.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+def get_data_from_row(row, keyword):
+    splitted_row = row.split(",")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+    if keyword == "name":
+        return splitted_row[0]
+    elif keyword == "email_address":
+        return splitted_row[1]
+    else:
+        return None
+
+def format_row(row):
+    return f"{row["name"]},{row["email"]},{row["year"]},{row["month"]},{row["day"]}"
+
+def read_birthdays(file_path):
+    try:
+        data = pandas.read_csv(file_path)
+        new_dict = {(row["month"], row["day"]): format_row(row) for (index, row) in data.iterrows()}
+        return new_dict
+    except FileNotFoundError:
+        print("File Not Found")
+        return {}
+    
+def read_letter_template(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            letter_template = file.read()
+
+        return letter_template
+    except FileNotFoundError:
+        print("File Not Found")
+        return []
+
+def create_personal_birthday_letter(name, random_letter_template):
+    personal_birthday_letter = random_letter_template.replace("[NAME]", name)
+    return personal_birthday_letter
+
+def send_birthday_letter_via_email(to_email, personal_birthday_letter):
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=to_email,
+                msg=f"Subject:Happy Birthday!\n\n{personal_birthday_letter}"
+            )
+
+            print("Email sending was successful!")
+    except:
+        print("The connection was wrong!")
+
+def check_birthdays(birthdays, letter_template):
+    for birthday_key in birthdays:
+        (birthday_month, birthday_day) = birthday_key
+        (today_month, today_day) = get_today_month_and_today_day()
+
+        if birthday_month == today_month and birthday_day == today_day:
+            name = get_data_from_row(birthdays[birthday_key], "name")
+            to_email = get_data_from_row(birthdays[birthday_key], "email_address")
+            personal_birthday_letter = create_personal_birthday_letter(name, letter_template)
+
+            send_birthday_letter_via_email(to_email, personal_birthday_letter)
+
+def run_app():
+    birthdays = read_birthdays("birthdays.csv")
+    random_letter_template = read_letter_template(f"letter_templates/letter_{randint(1, 4)}.txt")
+    check_birthdays(birthdays, random_letter_template)
+
+run_app()
